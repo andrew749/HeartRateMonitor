ea2c9f1c0cb23714a265dcefd027b8ebb22c922c
Andrew Codispoti <andrewcod749@gmail.com>
2015-02-08 22:21:01 -0500

    Implemented quick measurement button.

--- app/src/main/java/com/andrew749/heart_rate_monitor/Home.java
+++ app/src/main/java/com/andrew749/heart_rate_monitor/Home.java
@@ -1,24 +1,41 @@
 package com.andrew749.heart_rate_monitor;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.andrew749.tutorialcardlibrary.TutorialsCardActivity;
 
 /**
  * Created by andrew on 2/8/15.
  */
-public class Home extends Activity {
+public class Home extends Activity implements View.OnClickListener {
     //TODO run tutorial livrary to show user how to operate monitor
-
+    Button quickMeasurement;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
         //check to see if first time launching and launch tutorial
 //        TutorialsCardActivity activity = new TutorialsCardActivity();
+        quickMeasurement=(Button)findViewById(R.id.quickMeasurementButton);
+        quickMeasurement.setOnClickListener(this);
 
         //
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.quickMeasurementButton:
+                Intent intent=new Intent(this,HeartRateMonitor.class);
+                startActivity(intent);
+                break;
+
+
+        }
     }
 }
